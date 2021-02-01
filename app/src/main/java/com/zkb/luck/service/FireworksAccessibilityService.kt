c0fb3e59683c592df5d88a7cd25e0ea9426cef55
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2021-02-01 13:19:04 +0800

    initial

--- app/src/main/java/com/zkb/luck/service/FireworksAccessibilityService.kt
+++ app/src/main/java/com/zkb/luck/service/FireworksAccessibilityService.kt
@@ -15,18 +15,23 @@ import android.view.accessibility.AccessibilityNodeInfo
 class FireworksAccessibilityService : AccessibilityService() {
 
     private val TAG="kang"
-    private var count=0
-    private var maxSendMessageCount=10
-
+    @Volatile private var count=0
+    private var maxSendMessageCount=3
+    private var isStart=false;
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
 
         when (event?.eventType) {
             AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
                // find()
+                if(running) return
+                listenerCmd()
                 Log.d(TAG,"AccessibilityEvent WINDOW_STATE change:")
             }
             AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
                 Log.d(TAG,"AccessibilityEvent 内容change:")
+                if(running) return
+                listenerCmd()
+                if(!isStart) return
                 startFireworks();
             }
         }
@@ -34,12 +39,24 @@ class FireworksAccessibilityService : AccessibilityService() {
     }
 
 
-    private fun listenerCmd(){
 
+    @Volatile private var running = false
+    private fun listenerCmd(){
+        if(running) return
+        val list= rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/awv")
+            ?: return
+
+        val last=list.last();
+
+        if(last!=null){
+            isStart=true;
+            //last
+            Log.d(TAG,"last:"+last.text)
+        }
     }
 
     private fun  startFireworks(){
-
+        if(running) return
         if(count>maxSendMessageCount) return
         Log.d(TAG,"startFireworks:")
 
@@ -55,17 +72,27 @@ class FireworksAccessibilityService : AccessibilityService() {
         val sendBtn=findSendButton() ?: return
 
         val isSendSuccess= sendBtn.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-        if(!isSendSuccess){
+        if(isSendSuccess){
+            sendMsg(sendBtn)
            return
         }
-        //进入循环
-        while (count>maxSendMessageCount){
-            Thread.sleep(2000)
-            sendBtn.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-            count++
-        }
+
     }
 
+    @Synchronized  private  fun sendMsg(sendBtn : AccessibilityNodeInfo?){
+        if(running) return
+        Log.d(TAG,"startFireworks: //进入循环"+count)
+        running=true
+        //进入循环
+        while (count<maxSendMessageCount){
+            Thread.sleep(2000)
+            sendBtn?.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+            count++
+        }
+        Log.d(TAG,"startFireworks: //结束"+count)
+        count=0
+        running=false;
+    }
     private fun findSendButton(): AccessibilityNodeInfo? {
 
         val sendButton=  rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ay5")
