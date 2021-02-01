dc65a3096aca0d99a6883aa051c781f2cd41382c
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2021-02-01 15:47:11 +0800

    同步控制

--- app/src/main/java/com/zkb/luck/service/FireworksAccessibilityService.kt
+++ app/src/main/java/com/zkb/luck/service/FireworksAccessibilityService.kt
@@ -15,7 +15,13 @@ import android.view.accessibility.AccessibilityNodeInfo
 class FireworksAccessibilityService : AccessibilityService() {
     private val TAG="kang"
     @Volatile private var count=0
-    @Volatile private var running = false
+
+    /**
+     * 0 默认
+     * 1 处理中
+     * 2 发消息中
+     */
+    @Volatile private var runningType = 0
     @Volatile private var maxSendMessageCount=3
     private var isStart=false;
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
@@ -23,15 +29,17 @@ class FireworksAccessibilityService : AccessibilityService() {
         when (event?.eventType) {
             AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
                // find()
-                if(running) return
-                listenerCmd()
+                if(runningType!=0) return
+             //   listenerCmd()
                 Log.d(TAG,"AccessibilityEvent WINDOW_STATE change:")
             }
             AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
+
+                if(runningType!=0) return
+
                 Log.d(TAG,"AccessibilityEvent 内容change:")
-                if(running) return
-                listenerCmd()
-                if(!isStart) return
+               // listenerCmd()
+              //  if(!isStart) return
                 startFireworks();
             }
         }
@@ -43,9 +51,11 @@ class FireworksAccessibilityService : AccessibilityService() {
      * 奇怪拿不到聊天记录文本，难道text是绘制出来的？
      */
     private fun listenerCmd(){
-        if(running) return
+       // if(running) return
         val list= rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/awv")
             ?: return
+
+        if(list.isEmpty()) return
 
         val last=list.last();
 
@@ -56,13 +66,20 @@ class FireworksAccessibilityService : AccessibilityService() {
         }
     }
 
-    private fun  startFireworks(){
-        if(running) return
+    @Synchronized  private fun  startFireworks(){
+        if(runningType!=0) return
         if(count>maxSendMessageCount) return
         Log.d(TAG,"startFireworks:")
 
+        runningType=1
         //寻找输入框
-        val  editText= findInput() ?: return
+        val  editText= findInput()
+
+        if(editText == null){
+            runningType=0
+            return
+        }
+
 
         val arguments = Bundle()
         //输入框中输入礼花
@@ -73,7 +90,11 @@ class FireworksAccessibilityService : AccessibilityService() {
         editText.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments)
 
         //寻找发送按钮
-        val sendBtn=findSendButton() ?: return
+        val sendBtn=findSendButton()
+        if(sendBtn == null){
+            runningType=0
+            return
+        }
 
         //判断是否发送成功！
         val isSendSuccess= sendBtn.performAction(AccessibilityNodeInfo.ACTION_CLICK)
@@ -88,19 +109,21 @@ class FireworksAccessibilityService : AccessibilityService() {
     /**
      * 发送消息
      */
-    @Synchronized  private  fun sendMsg(sendBtn : AccessibilityNodeInfo?){
-        if(running) return
+     private  fun sendMsg(sendBtn : AccessibilityNodeInfo?){
+
+        if(runningType==2) return
         Log.d(TAG,"startFireworks: //进入循环"+count)
-        running=true
+        runningType=2
         //进入循环
         while (count<maxSendMessageCount){
-            Thread.sleep(2000)
+            Thread.sleep(1000)
             sendBtn?.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+            Log.d(TAG,"startFireworks: //ing"+count)
             count++
         }
         Log.d(TAG,"startFireworks: //结束"+count)
-        count=0
-        running=false;
+        count=0//可以用注释这里，到达次数就自动结束。
+        runningType=0;
     }
 
     /**
